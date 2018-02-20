6f04d71c757b1247a071efc3cfa12b5eb7673ab0
gustavo.henrique <pcasemirog@gmail.com>
2018-02-20 14:25:57 -0300

    subindo site

---
+++ index.php
@@ -0,0 +1,183 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/custom.css">
+
+    <title>Malves Estudio Criativo</title>
+  </head>
+  <style>
+    body{color:#fff;}
+    .container {
+    width: 100%;
+    padding-right: 150px;
+    padding-left: 150px;
+    margin-right: auto;
+    margin-left: auto;}
+    .home{
+    height: 40rem;
+    background-image: url('http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png');
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: ;
+    background-size: cover;
+  }
+  </style>
+  <body>
+    <section class="home">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="#"><img src="" alt="Malves"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto justify-content-end">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+      <div class="jumbotron text-center">
+      <h1 class="display-4">Hello, world!</h1>
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p class="lead">
+        <a class="btn btn-custom btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+    </div>
+
+  </section>
+
+  <section class="gatilho">
+    <div class="container">
+      <div class="row" style="padding: 50px 0">
+      <div class="col-sm" style="padding-top: 30px">
+        <h1 class="display-4">Hello, world!</h1>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p class="lead">
+          <a class="btn btn-dark btn-lg" href="#" role="button">Learn more</a>
+        </p>
+      </div>
+      <div class="col-sm">
+        <img style="width: 24rem; float: right;" src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt="">
+      </div>
+    </div>
+    </div>
+    
+  </section>
+
+  <section class="background-gradient">
+    <div class="container">
+      <div class="text-center header-malves">
+      <h3>Malves Estudio Critiavo</h3>
+      <p>It uses utility classes for typography and spacing</p>
+    </div>
+
+    <div style="width: 100%; height: 10rem; margin: 0 0 5rem 0; background-color: #444;">
+    </div>
+    
+    <div style="width: 100%; padding:0 0 5rem 0;">
+      <div class="row">
+
+      <div class="col-sm">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione fugit esse excepturi rem perferendis iure aperiam nostrum omnis recusandae, vero ut tempore nesciunt aliquam et cum debitis vel eveniet perspiciatis.
+      </div>
+      <div class="col-sm">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis magni ea sapiente doloribus ex debitis est quasi tempora sunt, placeat, culpa esse dolorem. Blanditiis voluptas excepturi fugit maiores architecto culpa?
+      </div>
+      
+    </div>
+    </div>
+    </div>
+
+    
+
+  </section>
+
+  <section>
+    <div class="row">
+
+      <div class="col-sm servicos">
+        <div class="jumbotron text-left">
+      <h1 class="display-4">Hello, world!</h1>
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p class="lead">
+        <a class="btn btn-custom btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+      </div>
+      <div class="col-sm servicos">
+        <div class="jumbotron text-left">
+      <h1 class="display-4">Hello, world!</h1>
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p class="lead">
+        <a class="btn btn-custom btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+      </div>
+      
+    </div>
+    <div class="row">
+
+      <div class="col-sm servicos">
+        <div class="jumbotron text-left">
+      <h1 class="display-4">Hello, world!</h1>
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p class="lead">
+        <a class="btn btn-custom btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+      </div>
+      <div class="col-sm servicos">
+        <div class="jumbotron text-left">
+      <h1 class="display-4">Hello, world!</h1>
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p class="lead">
+        <a class="btn btn-custom btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+      </div>
+      
+    </div>
+  </section>
+
+  <section class="portfolio">
+    <div class="container">
+      <div class="row">
+      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
+      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
+      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
+    </div>
+    <div class="row">
+      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
+      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
+      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
+    </div>
+
+    <p class="lead text-center">
+        <a class="btn btn-dark btn-lg" style="margin: 3rem 0;" href="#" role="button">Learn more</a>
+      </p>
+
+    </div>
+    
+  </section>
+    
+	
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>

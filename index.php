806d56c29cc6aed52219fee9ca89b1106af68da3
gustavo.henrique <pcasemirog@gmail.com>
2018-03-16 17:54:22 -0300

    alteracao total

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
     padding-left: 150px;
     margin-right: auto;
     margin-left: auto;}
+    
     .home{
     height: 40rem;
     background-image: url('http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png');
@@ -32,15 +33,30 @@
     <section class="home">
     <div class="container">
       <nav class="navbar navbar-expand-lg navbar-light">
-      <a class="navbar-brand" href="#"><img src="" alt="Malves"></a>
+      <a class="navbar-brand" href="#"><img style="height: 3rem;" src="img/logo-site.png" alt="Malves"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto justify-content-end">
+        <ul style="margin-left: 4.3rem;" class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="#">O ESTÙDIO</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">SERVIÇOS</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">PROJETOS</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">BLOG</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">CONTATO</a>
+          </li>
+          <li class="nav-item active">
+            <a id="orcamento" class="nav-link" href="#">ORÇAMENTO</a>
           </li>
         </ul>
       </div>
@@ -57,14 +73,14 @@
 
   </section>
 
-  <section class="gatilho">
+  <!--<section class="gatilho">
     <div class="container">
       <div class="row" style="padding: 50px 0">
       <div class="col-sm" style="padding-top: 30px">
         <h1 class="display-4">Hello, world!</h1>
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
         <p class="lead">
-          <a class="btn btn-dark btn-lg" href="#" role="button">Learn more</a>
+          <a class="btn btn-border-red btn-lg" href="#" role="button">Learn more</a>
         </p>
       </div>
       <div class="col-sm">
@@ -73,7 +89,81 @@
     </div>
     </div>
     
+  </section>-->
+  <section class="gatilho">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+
+          <div class="container">
+            <div class="row" style="padding: 50px 0">
+            <div class="col-sm" style="padding-top: 30px">
+              <h1 class="display-4">Hello, world!</h1>
+              <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+              <p class="lead">
+                <a class="btn btn-border-red btn-lg" href="#" role="button">Learn more</a>
+              </p>
+            </div>
+            <div class="col-sm">
+              <img style="width: 24rem; float: right;" src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt="">
+            </div>
+          </div>
+          </div>
+
+        </div>
+        <div class="carousel-item">
+
+          <div class="container">
+            <div class="row" style="padding: 50px 0">
+            <div class="col-sm" style="padding-top: 30px">
+              <h1 class="display-4">Hello, world!</h1>
+              <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+              <p class="lead">
+                <a class="btn btn-border-red btn-lg" href="#" role="button">Learn more</a>
+              </p>
+            </div>
+            <div class="col-sm">
+              <img style="width: 24rem; float: right;" src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt="">
+            </div>
+          </div>
+          </div>
+
+        </div>
+        <div class="carousel-item">
+
+          <div class="container">
+            <div class="row" style="padding: 50px 0">
+            <div class="col-sm" style="padding-top: 30px">
+              <h1 class="display-4">Hello, world!</h1>
+              <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+              <p class="lead">
+                <a class="btn btn-border-red btn-lg" href="#" role="button">Learn more</a>
+              </p>
+            </div>
+            <div class="col-sm">
+              <img style="width: 24rem; float: right;" src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt="">
+            </div>
+          </div>
+          </div>
+
+        </div>
+        
+      <!--<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>-->
+    </div>
   </section>
+  
 
   <section class="background-gradient">
     <div class="container">
@@ -106,21 +196,21 @@
   <section>
     <div class="row">
 
-      <div class="col-sm servicos">
+      <div id="id" class="col-sm servicos">
         <div class="jumbotron text-left">
       <h1 class="display-4">Hello, world!</h1>
       <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
       <p class="lead">
-        <a class="btn btn-custom btn-lg" href="#" role="button">Learn more</a>
+        <a class="btn btn-border-white btn-lg" href="#" role="button">Learn more</a>
       </p>
     </div>
       </div>
-      <div class="col-sm servicos">
+      <div id="web" class="col-sm servicos">
         <div class="jumbotron text-left">
       <h1 class="display-4">Hello, world!</h1>
       <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
       <p class="lead">
-        <a class="btn btn-custom btn-lg" href="#" role="button">Learn more</a>
+        <a class="btn btn-border-white btn-lg" href="#" role="button">Learn more</a>
       </p>
     </div>
       </div>
@@ -128,21 +218,21 @@
     </div>
     <div class="row">
 
-      <div class="col-sm servicos">
+      <div id="redes" class="col-sm servicos">
         <div class="jumbotron text-left">
       <h1 class="display-4">Hello, world!</h1>
       <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
       <p class="lead">
-        <a class="btn btn-custom btn-lg" href="#" role="button">Learn more</a>
+        <a class="btn btn-border-white btn-lg" href="#" role="button">Learn more</a>
       </p>
     </div>
       </div>
-      <div class="col-sm servicos">
+      <div id="loja" class="col-sm servicos">
         <div class="jumbotron text-left">
       <h1 class="display-4">Hello, world!</h1>
       <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
       <p class="lead">
-        <a class="btn btn-custom btn-lg" href="#" role="button">Learn more</a>
+        <a class="btn btn-border-white btn-lg" href="#" role="button">Learn more</a>
       </p>
     </div>
       </div>
@@ -153,26 +243,112 @@
   <section class="portfolio">
     <div class="container">
       <div class="row">
-      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
-      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
-      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
+      <div class='col-sm'><img src="img/portfolio-1.jpg" alt=""></div>
+      <div class='col-sm'><img src="img/portfolio-2.jpg" alt=""></div>
+      <div class='col-sm'><img src="img/portfolio-3.jpg" alt=""></div>
     </div>
     <div class="row">
-      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
-      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
-      <div class='col-sm'><img src="http://res.cloudinary.com/estudiomalves/image/upload/v1518726315/bg-corrida_qfaywo.png" alt=""></div>
+      <div class='col-sm'><img src="img/portfolio-4.jpg" alt=""></div>
+      <div class='col-sm'><img src="img/portfolio-5.jpg" alt=""></div>
+      <div class='col-sm'><img src="img/portfolio-6.jpg" alt=""></div>
     </div>
 
     <p class="lead text-center">
-        <a class="btn btn-dark btn-lg" style="margin: 3rem 0;" href="#" role="button">Learn more</a>
+        <a class="btn btn-border-red btn-lg" style="margin: 3rem 0;" href="#" role="button">Learn more</a>
       </p>
 
     </div>
     
   </section>
-    
-	
 
+  <section class="clientes">
+    <div class="container">
+      <div>
+        
+      </div>
+    </div>
+  </section>
+
+  <section class="testemunho">
+    <div class="container">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators" style="bottom: -70px;">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+              <div class="row">
+                <div class="col-sm">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor nostrum, eaque officia iure saepe, tempore quam voluptatibus architecto molestias vero pariatur sint aperiam minus dolorem unde amet eius quas ducimus?</p>
+                  <p>nome</p>
+                  <p>cargo</p>
+                </div>
+                <div class="col-sm">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor nostrum, eaque officia iure saepe, tempore quam voluptatibus architecto molestias vero pariatur sint aperiam minus dolorem unde amet eius quas ducimus?</p>
+                  <p>nome</p>
+                  <p>cargo</p>
+                </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="blog">
+    
+  </section>
+
+  <section class="contato background-gradient">
+    <div class="container text-center">
+      <h3 class="text-center">Contato</h3>
+      <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid totam at sint repellendus</p>
+      <form action="">
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <input type="email" class="form-control form-costumize" id="inputEmail4" placeholder="Nome">
+          </div>
+          <div class="form-group col-md-6">
+            <input type="password" class="form-control form-costumize" id="inputPassword4" placeholder="Email">
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <input type="email" class="form-control form-costumize" id="inputEmail4" placeholder="Telefone">
+          </div>
+          <div class="form-group col-md-6">
+            <input type="password" class="form-control form-costumize" id="inputPassword4" placeholder="Celular">
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <input type="email" class="form-control form-costumize" id="inputEmail4" placeholder="Empresa">
+          </div>
+          <div class="form-group col-md-6">
+            <input type="password" class="form-control form-costumize" id="inputPassword4" placeholder="Assunto">
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-12">
+            <textarea class="form-control form-costumize" name="" id="" cols="30" rows="2"></textarea>
+          </div>
+        </div>
+          <a class="btn btn-border-white btn-lg" href="#" role="button">Learn more</a>
+      </form>
+
+      <p style="margin: 6rem 0px">contato@estudiomalves.com<br>
+      +55(11)9.9015-4774 | +5(11)9.8765-2242</p>
+      
+      <div style="border-top: 1px solid #fff;">
+        <div style="display: inline-flex;">
+          <p class="text-left" style="font-size: 14px">@Copyright 2018 Malves Estúdio Criativo - Todos so direitos Reservados</p>
+          <img style="height: 2rem;" src="img/logo-site.png" alt="Malves">
+        </div>
+      
+      </div>
+    </div>
+  </section>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
